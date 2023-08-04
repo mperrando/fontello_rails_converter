98d9e30ca00729e20343436c57741029e3ee11f3
Marco Perrando <marco.perrando@gmail.com>
2023-08-04 11:21:02 +0200

    Removed url->fint-rul conversion for Sprockets 4.

--- lib/fontello_rails_converter/cli.rb
+++ lib/fontello_rails_converter/cli.rb
@@ -101,9 +101,9 @@ module FontelloRailsConverter
       def convert_for_asset_pipeline(content)
         # asset URLs
         content.gsub! /\.\.\/font\//, ""
-        content.gsub!(/url\(([^\(]+)\)/) do |m|
-          $1.include?("application/octet-stream") ? "url(#{$1})" : "font-url(#{$1})"
-        end
+        #content.gsub!(/url\(([^\(]+)\)/) do |m|
+        #  $1.include?("application/octet-stream") ? "url(#{$1})" : "font-url(#{$1})"
+        #end
       end
 
       def convert_for_webpack(content)
