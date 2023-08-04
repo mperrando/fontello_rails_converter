0cf87617552af7f8dadd880f0a8a2f22df098fd6
Marco Perrando <marco.perrando@gmail.com>
2023-08-04 09:29:49 +0200

    Add compatibility with sprockets 4.
    See https://github.com/FortAwesome/font-awesome-sass/commit/b8dd32cf3bcd5d14c12543d767f8db68b31a6a9b

--- lib/fontello_rails_converter/railtie.rb
+++ lib/fontello_rails_converter/railtie.rb
@@ -4,7 +4,9 @@ module FontelloRailsConverter
     initializer "fontello_rails_converter.setup" do |app|
       if app.config.respond_to? (:assets)
         app.config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')
-        app.config.assets.precompile << /\.(?:svg|eot|woff|woff2|ttf)$/
+        %w(eot svg ttf woff woff2).each do |ext|
+          app.config.assets.precompile << "font-awesome/fontawesome-webfont.#{ext}"
+        end
       end
     end
 
